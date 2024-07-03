f71f0a2eaeff4da9e274b062056132e3a0853f51
vishal kumar <leader.vishalkumar@gmail.com>
2024-07-03 13:35:11 +0530

    feat: gulp-error handling
    feat: gulp-error handling and added badge component

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,34 @@
-const { src, dest, watch, series}= require('gulp')
-const sass = require('gulp-sass')(require('sass'))
-const purgecss = require('gulp-purgecss')
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
+const plumber = require('gulp-plumber');
+const notify = require('gulp-notify');
 
 function buildStyles() {
-    return src('rapidstyle/**/*.scss')
-    .pipe(sass())
-    .pipe(purgecss({content: ['*.html']}))
-    .pipe(dest('css'))
+  return src("sass/**/*.scss")
+    .pipe(
+      plumber({
+        // Handle errors gracefully without stopping Gulp
+        errorHandler: notify.onError("Error: <%= error.message %>"),
+      })
+    )
+    .pipe(
+      sass({
+        // Compile SCSS to CSS
+        outputStyle: "compressed", // Minimize the output CSS
+      })
+    )
+    .pipe(
+      purgecss({
+        // Remove unused CSS based on the content of HTML files
+        content: ["*.html"],
+      })
+    )
+    .pipe(dest("css")); // Save the output CSS files in the 'css' directory
 }
 
 function watchTask() {
-    watch(['rapidstyle/**/*.scss', '*.html'], buildStyles)
+  watch(["sass/**/*.scss", "*.html"], buildStyles);
 }
 
-exports.default = series(buildStyles, watchTask)
+exports.default = series(buildStyles, watchTask);
